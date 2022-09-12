54b2e2df29faafcb6f6c92b0109baf12406566f7
Emma Velazquez <emmasiren24@gmail.com>
2022-09-12 16:51:52 -0700

    Formatted moment.js hour to 12hour time. Wrote forEach loop with if else for color coding time slots.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,30 +2,42 @@ var today = moment().format('dddd, MMMM Do');
 $("#currentDay").text(today);
 
 var currentTime = moment().hour();
-var nineAm = $('.description');
-var before = moment().startOf('hour').fromNow();
-var after = moment().endOf('hour').fromNow();
-var hour = $('.hour');
+var twelveHour = moment().format("h");
+// var nineAm = $('.test');
+// var before = moment().startOf('hour').fromNow();
+// var after = moment().endOf('hour').fromNow();
 
-console.log(currentTime);
-console.log(before);
-console.log(after);
+var momentInt = parseInt(twelveHour);
+console.log(momentInt);
+// console.log(before);
+// console.log(after);
 
-// hour.forEach(element => {
-// var hourInt = parseInt($(hour).text());
-// console.log(hourInt);
-// });
+$('.hour').each(function() {
+var hourInt = parseInt($(this).text());
+console.log(hourInt);
 
-if (before) {
-    console.log("grey");
-    nineAm.addClass('past');
-} 
-else 
-if (after) {
-    console.log("green");
-    nineAm.addClass('future');
-} 
-else {
-    console.log("red");
-    nineAm.addClass('present');
+if (hourInt === momentInt) {
+    console.log("present");
+    $('.test').addClass('present');
+} else
+if (hourInt < momentInt) {
+    console.log("past")
+    $('.test').addClass('past');
+    $('.test').removeClass('present');
 }
+
+});
+
+// if (before) {
+//     console.log("grey");
+//     nineAm.addClass('past');
+// } 
+// else 
+// if (after) {
+//     console.log("green");
+//     nineAm.addClass('future');
+// } 
+// else {
+//     console.log("red");
+//     nineAm.addClass('present');
+// }
