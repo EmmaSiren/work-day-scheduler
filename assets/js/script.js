7fe07cc9134b3b89fe7debb461bff0f4a7b11ff0
Emma Velazquez <emmasiren24@gmail.com>
2022-09-12 18:31:46 -0700

    Worked on getting value from user input in textarea

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,42 +2,29 @@ var today = moment().format('dddd, MMMM Do');
 $("#currentDay").text(today);
 
 var currentTime = moment().hour();
-var twelveHour = moment().format("h");
-// var nineAm = $('.test');
-// var before = moment().startOf('hour').fromNow();
-// var after = moment().endOf('hour').fromNow();
+console.log(currentTime);
+// console.log($('.description').attr('class').split(' ')[2]);
 
-var momentInt = parseInt(twelveHour);
-console.log(momentInt);
-// console.log(before);
-// console.log(after);
+$('.description').each(function() {
+    var hourInt = parseInt($(this).attr("class").split(' ')[2]);
+    console.log(hourInt);
 
-$('.hour').each(function() {
-var hourInt = parseInt($(this).text());
-console.log(hourInt);
-
-if (hourInt === momentInt) {
-    console.log("present");
-    $('.test').addClass('present');
-} else
-if (hourInt < momentInt) {
-    console.log("past")
-    $('.test').addClass('past');
-    $('.test').removeClass('present');
-}
-
+    if (hourInt === currentTime) {
+        console.log("present");
+        $(this).addClass('present');
+    } else if (hourInt < currentTime) {
+        console.log("past")
+        $(this).addClass('past');
+    } else {
+        console.log('future');
+        $(this).addClass('future');
+    }
 });
 
-// if (before) {
-//     console.log("grey");
-//     nineAm.addClass('past');
-// } 
-// else 
-// if (after) {
-//     console.log("green");
-//     nineAm.addClass('future');
-// } 
-// else {
-//     console.log("red");
-//     nineAm.addClass('present');
-// }
+
+$('.saveBtn').click(function (){
+    var taskContent = $('.description').val();
+    console.log(taskContent);
+
+    // localStorage.setItem('task', taskContent);
+});
