9ac539e5366ada819a19a5f860bc86126a5ef389
Emma Velazquez <emmasiren24@gmail.com>
2022-09-14 16:06:17 -0700

    Finalized all files. Added comments. Wrote ReadMe.

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
-# work-day-scheduler
+# Work Day Scheduler
 
+![JS](https://img.shields.io/badge/JavaScript-17%25-yellow)
+![HTML](https://img.shields.io/badge/HTML-65%25-orange)
+![CSS](https://img.shields.io/badge/CSS-18%25-blueviolet)
+
+## Description
+
+In order to build a successful scheduling application for an employee with an average work day, this application needs to give the user the current date and time, allow user to input tasks for each hour, and save those tasks even after page refresh. While HTML and CSS starter code were provided, they lacked time slots or the correct usage of Bootstrap to format the page aesthetically. After including enough time slots for the average work day, I used Bootstrap classes in order to better present the layout of the webpage. I also included classes for the JavaScript to select from in order to function alongside Moment.js API to give the user accurate date and time information. Moving on to the JavaScript code, I began by setting variables for the date, the hour, and each hour class. By parsing the integer of each hour class I was able to compare that to the current hour using Moment.js and color code the time blocks to grey (past), red (present), or green (future). Lastly, I set the hour time blocks and user input tasks as key: value pairs to local storage on the save button click and made sure they persisted even after page refresh. This application served as great practice for using a CSS framework like Bootstrap and APIs such as Jquery and Moment.js. 
+
+#
+## Usage
+
+To view the website application please follow the provided link: 
 https://emmasiren.github.io/work-day-scheduler/
+
+The following image shows the web application when loaded correctly.
+![img](assets/images/img.jpg)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,12 @@
+// Set current day's date using moment.js
 var today = moment().format('dddd, MMMM Do');
+// Set current hour using moment.js
 var currentTime = moment().hour();
 
+// Display current date in jumbotron
 $("#currentDay").text(today);
 
-$('.description').each(function() {
-    var hourInt = parseInt($(this).attr("class").split(' ')[2]);
-
-    if (hourInt === currentTime) {
-        $(this).addClass('present');
-    } else if (hourInt < currentTime) {
-        $(this).addClass('past');
-    } else {
-        $(this).addClass('future');
-    };
-});
-
-$('.saveBtn').click(function (){
-    var hour = $(this).siblings('.description').attr("class").split(' ')[2];
-    var task = $(this).siblings('.description').val();
-
-    localStorage.setItem(hour, task);
-});
-
+// Displays any tasks previously saved to local storage upon page load/refresh
 $('.9').text(localStorage.getItem('9'));
 $('.10').text(localStorage.getItem('10'));
 $('.11').text(localStorage.getItem('11'));
@@ -31,3 +16,29 @@ $('.14').text(localStorage.getItem('14'));
 $('.15').text(localStorage.getItem('15'));
 $('.16').text(localStorage.getItem('16'));
 $('.17').text(localStorage.getItem('17'));
+
+// Loop through each time block
+$('.description').each(function() {
+    // Grab each time block's hour class after 'description' class and set it as an integer
+    var hourInt = parseInt($(this).attr("class").split(' ')[2]);
+
+    // Each time block color coded based on past, present, or future
+    if (hourInt === currentTime) {
+        $(this).addClass('present');
+    } else if (hourInt < currentTime) {
+        $(this).addClass('past');
+    } else {
+        $(this).addClass('future');
+    };
+});
+
+// Save user input task to corresponding hour in local storage 
+$('.saveBtn').click(function (){
+    // Each time slot corresponds to it's save button
+    var hour = $(this).siblings('.description').attr("class").split(' ')[2];
+    // Each text area's input value
+    var task = $(this).siblings('.description').val();
+
+    // Sets time slot key to task value to local storage
+    localStorage.setItem(hour, task);
+});
