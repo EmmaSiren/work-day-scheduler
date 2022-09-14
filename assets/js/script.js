e656ab288352689daa7ab6259a9c55a82157c33f
Emma Velazquez <emmasiren24@gmail.com>
2022-09-14 00:57:00 -0700

    Set hour:task key value pair to local storage. Pulled info from local storage and displayed.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,33 @@
 var today = moment().format('dddd, MMMM Do');
-$("#currentDay").text(today);
-
 var currentTime = moment().hour();
-console.log(currentTime);
-// console.log($('.description').attr('class').split(' ')[2]);
-var test = $('.description')
 
+$("#currentDay").text(today);
 
 $('.description').each(function() {
     var hourInt = parseInt($(this).attr("class").split(' ')[2]);
-    console.log(hourInt);
 
     if (hourInt === currentTime) {
-        console.log("present");
         $(this).addClass('present');
     } else if (hourInt < currentTime) {
-        console.log("past")
         $(this).addClass('past');
     } else {
-        console.log('future');
         $(this).addClass('future');
-    }
+    };
 });
-
 
 $('.saveBtn').click(function (){
-    var taskContent = $(this).siblings('.description').val();
-    console.log(taskContent);
+    var hour = $(this).siblings('.description').attr("class").split(' ')[2];
+    var task = $(this).siblings('.description').val();
 
-    // localStorage.setItem('task', taskContent);
+    localStorage.setItem(hour, task);
 });
+
+$('.9').text(localStorage.getItem('9'));
+$('.10').text(localStorage.getItem('10'));
+$('.11').text(localStorage.getItem('11'));
+$('.12').text(localStorage.getItem('12'));
+$('.13').text(localStorage.getItem('13'));
+$('.14').text(localStorage.getItem('14'));
+$('.15').text(localStorage.getItem('15'));
+$('.16').text(localStorage.getItem('16'));
+$('.17').text(localStorage.getItem('17'));
